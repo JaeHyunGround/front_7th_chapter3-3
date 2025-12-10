470a0b411630e8e50f3f55ea23ae65a92b1acfc8
JaeHyunGround <gothddlek@naver.com>
2025-12-10 17:57:15 +0900

    feat : 댓글 도메인에 대한 entities 레이어 생성
    - api 호출 로직
    - 타입
    을 entities 레이어에 생성했습니다.

---
+++ src/entities/comment/index.ts
@@ -0,0 +1,9 @@
+export { commentApi } from "./api/comment-api"
+export type {
+  Comment,
+  CommentUser,
+  CommentsResponse,
+  CreateCommentDto,
+  UpdateCommentDto,
+  UpdateCommentLikesDto,
+} from "./model/types"
