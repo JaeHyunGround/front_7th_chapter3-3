f9aa8fb27b94a4ac060e8dafcf314624f9913ceb
JaeHyunGround <gothddlek@naver.com>
2025-12-10 19:18:39 +0900

    refactor : app/providers 폴더 생성 및 쿼리 프로바이더 적용 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { BrowserRouter as Router } from "react-router-dom"
 import { Header, Footer } from "@/widgets/layout/ui/index.ts"
 import PostsManagerPage from "./pages/PostsManagerPage.tsx"
+import { QueryProvider } from "@/app/providers"
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <QueryProvider>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </QueryProvider>
   )
 }
 
