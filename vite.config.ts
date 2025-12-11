9fb169dc1d7081731737ec448686904553fb4e8e
JaeHyunGround <gothddlek@naver.com>
2025-12-12 02:04:37 +0900

    feat : 배포 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react"
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/front_7th_chapter3-3/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,11 +14,10 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: 'https://jsonplaceholder.typicode.com',
         target: "https://dummyjson.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
-})
+}))
